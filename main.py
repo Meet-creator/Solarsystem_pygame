ead4b46fe044461fa92a4cb40aa3ce3bf8fbd05a
Meet-creator <73884094+Meet-creator@users.noreply.github.com>
2022-02-24 19:52:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,145 @@
 import pygame
 import math
-
-angle = 0
-
-#initialising the pygame
 pygame.init()
 
-# create screen
-screen = pygame.display.set_mode((1920, 1080))
+WIDTH, HEIGHT =  1080, 1024
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Solar System")
 
-# Title and Icon
-pygame.display.set_caption("Solar system")
-icon = pygame.image.load('solarsystem.png')
-pygame.display.set_icon(icon)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
+JUPITER = (183, 74, 72)
+SATURN = (255, 215, 0)
+URANUS = (76, 185, 234)
+NEPTUNE = (19, 48, 42)
 
-# Planet class
-class Planets:
+FONT = pygame.font.SysFont("monospace", 16)
 
-    def __init__(self, planet,radius,size):
-        self.planet = planet
-        self.radius = radius
-        self.size = size
+class Planet:
+	AU = 149.6e6 * 1000
+	G = 6.67428e-11
+	SCALE = 200 / AU  # 1AU = 100 pixels
+	TIMESTEP = 3600*24 # 1 day
+
+	def __init__(self, name,  x, y, radius, color, mass):
+		self.name = name
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.mass = mass
+
+		self.orbit = []
+		self.sun = False
+		self.distance_to_sun = 0
+
+		self.x_vel = 0
+		self.y_vel = 0
+
+	def draw(self, win):
+		x = self.x * self.SCALE + WIDTH / 2
+		y = self.y * self.SCALE + HEIGHT / 2
+
+		if len(self.orbit) > 2:
+			updated_points = []
+			for point in self.orbit:
+				x, y = point
+				x = x * self.SCALE + WIDTH / 2
+				y = y * self.SCALE + HEIGHT / 2
+				updated_points.append((x, y))
+
+			pygame.draw.lines(win, self.color, False, updated_points, 2)
+
+		pygame.draw.circle(win, self.color, (x, y), self.radius)
+		
+		if not self.sun:
+			distance_text = FONT.render(f"{self.name}", 1, WHITE)
+			win.blit(distance_text, (x - distance_text.get_width()/2, y + distance_text.get_height()/2))
+
+	def attraction(self, other):
+		other_x, other_y = other.x, other.y
+		distance_x = other_x - self.x
+		distance_y = other_y - self.y
+		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+		if other.sun:
+			self.distance_to_sun = distance
+
+		force = self.G * self.mass * other.mass / distance**2
+		theta = math.atan2(distance_y, distance_x)
+		force_x = math.cos(theta) * force
+		force_y = math.sin(theta) * force
+		return force_x, force_y
+
+	def update_position(self, planets):
+		total_fx = total_fy = 0
+		for planet in planets:
+			if self == planet:
+				continue
+
+			fx, fy = self.attraction(planet)
+			total_fx += fx
+			total_fy += fy
+
+		self.x_vel += total_fx / self.mass * self.TIMESTEP
+		self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+		self.x += self.x_vel * self.TIMESTEP
+		self.y += self.y_vel * self.TIMESTEP
+		self.orbit.append((self.x, self.y))
 
 
-    def draw(self):
-        global angle
-        global sunX
-        global sunY
-        theta = math.radians(angle)
-        planet_coords = sunX + self.radius * math.cos(theta), sunY + self.radius * math.sin(theta)
-        angle += 0.01
-        pygame.draw.circle(screen, (173, 173, 174), [sunX + 16, sunY + 16], self.radius, 2)
-        screen.blit(self.planet, (planet_coords[0] + self.size, planet_coords[1] + self.size))
+def main():
+	run = True
+	clock = pygame.time.Clock()
+
+	sun = Planet("Sun", 0, 0, 30, YELLOW, 1.98892 * 10**30)
+	sun.sun = True
+
+	earth = Planet("Earth", -1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+	earth.y_vel = 29.783 * 1000
+
+	mars = Planet("Mars", -1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+	mars.y_vel = 24.077 * 1000
+
+	mercury = Planet("Mercury", 0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+	mercury.y_vel = -47.4 * 1000
+
+	venus = Planet("Venus", 0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+	venus.y_vel = -35.02 * 1000
+
+	jupiter = Planet("Jupiter", 5.2 * Planet.AU, 0, 24, JUPITER, 1.898 * 10**27)
+	jupiter.y_vel = -13.06 * 1000
+
+	saturn = Planet("Saturn", 9.5 * Planet.AU, 0, 20, SATURN, 5.683 * 10**26)
+	saturn.y_vel = 9.68 * 1000
+
+	uranus = Planet("Uranus", 19.8 * Planet.AU, 0, 18, URANUS, 8.681 * 10**25)
+	uranus.y_vel = 6.80 * 1000
+
+	neptune = Planet("Neptune", 30 * Planet.AU, 0, 17, NEPTUNE, 1.024 * 10**26)
+	neptune.y_vel = 5.43 * 1000
+
+	planets = [sun, earth, mars, mercury, venus, jupiter, saturn, uranus, neptune]
+
+	while run:
+		clock.tick(60)
+		WIN.fill((0, 0, 0))
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		for planet in planets:
+			planet.update_position(planets)
+			planet.draw(WIN)
+
+		pygame.display.update()
+
+	pygame.quit()
 
 
-
-# Sun
-sun = pygame.image.load("sun.png")
-sunX = 944
-sunY = 524
-
-def draw_sun():
-    screen.blit(sun, (sunX, sunY))
-
-# Mercury
-mercury = pygame.image.load("mercury.png")
-Mercury = Planets(mercury,100,12)
-
-# Venus
-venus = pygame.image.load("venus.png")
-Venus = Planets(venus,130,16)
-
-# Earth
-earth = pygame.image.load("earth.png")
-Earth = Planets(earth,160,16)
-
-# Mars
-mars = pygame.image.load("mars.png")
-Mars = Planets(mars,200,16)
-
-# Jupiter
-jupiter = pygame.image.load("jupiter.png")
-Jupiter = Planets(jupiter,300,32)
-
-# Saturn
-saturn = pygame.image.load("saturn.png")
-Saturn = Planets(saturn,380,16)
-
-# Uranus
-uranus = pygame.image.load("uranus.png")
-Uranus = Planets(uranus,450,16)
-
-# Neptune
-neptune = pygame.image.load("neptune.png")
-Neptune = Planets(neptune,550,12)
-
-# Game Loop
-running = True
-while running:
-
-    # Background Color
-    screen.fill((10, 10, 10))
-
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT:
-            running = False
-
-    draw_sun()
-    Mercury.draw()
-    Venus.draw()
-    Earth.draw()
-    Mars.draw()
-    Jupiter.draw()
-    Saturn.draw()
-    Uranus.draw()
-    Neptune.draw()
-
-    pygame.display.update()
+main()
